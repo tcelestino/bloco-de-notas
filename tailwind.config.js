d3570b307f8c332007dca74f6813ff876039ebca
Tiago Celestino <tcelestino@gmail.com>
2025-02-25 16:58:12 -0300

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.njk', './src/**/*.svg', './src/assets/js/*.js'],
+  screens: {
+    sm: '576px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
