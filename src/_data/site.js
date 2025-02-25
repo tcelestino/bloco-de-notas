a435435d2083f6d39e0f35b8a00d05ac0da192a2
Tiago Celestino <tcelestino@gmail.com>
2025-02-25 16:59:44 -0300

    feat: create site data

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Bloco de Notas por Tiago Celestino',
+  description: 'Blog pessoal do entusiata de tecnologia Tiago Celestino',
+  keywords: ['eleventy', 'tecnologia', 'programação', 'gestão'],
+  url: 'https://bloco-de-notas.github.io/bloco-de-notas/',
+  paginate: 6,
+};
