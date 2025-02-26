e8baa4790ea953363ff57fa2128a1784013bc93d
Tiago Celestino <tcelestino@gmail.com>
2025-02-26 16:42:42 -0300

    feat: set meta tags and load font via google fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const dataSite = require('./src/_data/site.js');
+const menuData = require('./src/_data/menu.js');
 
 module.exports = (config) => {
   config.addPassthroughCopy('src/assets/images/**/*');
@@ -15,11 +17,15 @@ module.exports = (config) => {
 
   config.addPlugin(syntaxHighlight);
 
+  // TODO: _data folder is not working
+  config.addGlobalData('site', dataSite);
+  config.addGlobalData('menu', menuData);
+
   return {
     dir: {
       input: 'src',
       output: './_site',
-      layouts: '_includes',
+      includes: '_includes',
     },
     templateFormats: ['md', 'njk', 'html'],
     dataTemplateEngine: 'njk',
