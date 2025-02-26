a3dfb6f08c42b3798ec4b3bcb07e161b06d4d777
Tiago Celestino <tcelestino@gmail.com>
2025-02-26 16:41:54 -0300

    refactor: update and remove files

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,14 @@
 module.exports = {
   title: 'Bloco de Notas por Tiago Celestino',
-  description: 'Blog pessoal do entusiata de tecnologia Tiago Celestino',
-  keywords: ['eleventy', 'tecnologia', 'programação', 'gestão'],
-  url: 'https://bloco-de-notas.github.io/bloco-de-notas/',
-  paginate: 6,
+  description: 'Blog pessoal de um entusiasta de tecnologia',
+  keywords: [
+    '11yt',
+    'tecnologia',
+    'programação',
+    'gestão',
+    'desenvolvimento web',
+  ],
+  url: 'https://tcelestino.github.io/bloco-de-notas',
+  author: 'Tiago Celestino',
+  paginate: 3,
 };
