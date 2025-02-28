a372fb67fb2f5e66cb954138a1121da59e6081ab
Tiago Celestino <tcelestino@gmail.com>
2025-02-28 17:50:11 -0300

    refactor: add page language

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,15 +1,9 @@
 export default {
   title: 'Bloco de Notas por Tiago Celestino',
   description: 'Blog pessoal de um entusiasta de tecnologia e afins',
-  keywords: [
-    '11yt',
-    'tecnologia',
-    'programação',
-    'gestão',
-    'desenvolvimento web',
-  ],
+  keywords: ['11yt', 'tecnologia', 'programação', 'gestão', 'desenvolvimento web'],
   url: 'https://tcelestino.github.io/bloco-de-notas',
   author: 'Tiago Celestino',
-  lang: 'pt-BR',
+  language: 'pt-BR',
   paginate: 3,
 };
