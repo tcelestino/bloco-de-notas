bc39209cc2e64e94c33932ea6863367a39742d5f
Tiago Celestino <tcelestino@gmail.com>
2025-03-01 13:16:23 -0300

    feat: settings to posts

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,3 +1,12 @@
 export default {
-  permalink: "/{{ page.date | date: '%Y/%m'}}/{{ page.fileSlug }}/",
+  eleventyComputed: {
+    permalink: (data) => {
+      `/${data.page.date.getFullYear()}/${String(data.page.date.getMonth() + 1).padStart(2, '0')}/${
+        data.page.fileSlug
+      }.html`;
+    },
+  },
+  layout: 'post',
+  tags: ['tech'],
+  title: 'Sem título',
 };
